8bc3f8597a240a3971193be022c2d3e81468d9b6
Jacob Hayes <108651219+sejah@users.noreply.github.com>
2025-02-21 01:33:17 +1100

    first react commit

---
+++ precache-manifest.a2a46aca21cbf13152ccfa73a27bd45c.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c04e681115f17f97621cdbd37af8f7d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "92dfe5806bfdcf3b6483",
+    "url": "/static/css/main.19c98d2f.chunk.css"
+  },
+  {
+    "revision": "56f1224220a5f06f9596",
+    "url": "/static/js/2.e19d9236.chunk.js"
+  },
+  {
+    "revision": "92dfe5806bfdcf3b6483",
+    "url": "/static/js/main.ac7692be.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "d0b2ba9ba0932262faadcb2b96288e40",
+    "url": "/static/media/background.d0b2ba9b.jpg"
+  },
+  {
+    "revision": "2a45c381cf87175fb92c8bc55202d529",
+    "url": "/static/media/email-icon.2a45c381.svg"
+  },
+  {
+    "revision": "9dfff35ba1788bd2a3b990a18a7ce2b2",
+    "url": "/static/media/hospo.9dfff35b.jpg"
+  },
+  {
+    "revision": "eaaa8ba100db683015c5ff170841fd9b",
+    "url": "/static/media/logo-amazon.eaaa8ba1.svg"
+  },
+  {
+    "revision": "587cc2ad57ad89843481c5bd3046c539",
+    "url": "/static/media/logo-css3.587cc2ad.svg"
+  },
+  {
+    "revision": "ecaefd058c8b50de9e3070d4abab72e9",
+    "url": "/static/media/logo-github.ecaefd05.svg"
+  },
+  {
+    "revision": "da0088d155c90cd9c8fa0c4dbaaee0c6",
+    "url": "/static/media/logo-html5.da0088d1.svg"
+  },
+  {
+    "revision": "950b621dce5f99f557c4a2f604ae528f",
+    "url": "/static/media/logo-javascript.950b621d.svg"
+  },
+  {
+    "revision": "b5cd8be80f156bc337ba2af2f1f5fa09",
+    "url": "/static/media/logo-linkedin.b5cd8be8.svg"
+  },
+  {
+    "revision": "ba9dcf6cc0e651233dd91d1363e33ef5",
+    "url": "/static/media/logo-nodejs.ba9dcf6c.svg"
+  },
+  {
+    "revision": "082243104d5906293a3b7c4970f93294",
+    "url": "/static/media/logo-python.08224310.svg"
+  },
+  {
+    "revision": "2b725c612e993a2c6b458177ffb4a8b5",
+    "url": "/static/media/logo-react.2b725c61.svg"
+  },
+  {
+    "revision": "18ac84bde1b86e61f557c5bad68fe50a",
+    "url": "/static/media/retail.18ac84bd.jpg"
+  },
+  {
+    "revision": "0d7f26590be726ce262ff619eb3455a4",
+    "url": "/static/media/workplace.0d7f2659.jpg"
+  }
+]);
